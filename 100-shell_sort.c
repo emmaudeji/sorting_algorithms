3906753f68b7c05725dbcfed99517906431f6e34
emmaudeji <gogreneinfo@gmail.com>
2022-01-10 07:39:38 -0800

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,40 +1,41 @@
 #include "sort.h"
 
 /**
-  * shell_sort - A function tat sorts 
-  * an array using shell algorithm.
-  * 
-  * @array: The array to sort.
-  * @size: The length of the array.
-  * 
-  * Return: void.
-  */
+ * shell_sort - sorts an array of integers in ascending order
+ * @array: Array of integers
+ * @size: Size of the array
+ * Return - void
+ */
 void shell_sort(int *array, size_t size)
 {
-	unsigned int i = 0, j = 0, gap = 0;
-	int aux = 0;
+	size_t i, space = 1, j;
+	int tmp; /*tmporal variable for swap*/
 
-	if (array == NULL || size < 2)
+	if (!array || size < 2)
 		return;
 
-	while (gap < size / 3)
-		gap = gap * 3 + 1;
-
-	for (; gap > 0; gap = (gap - 1) / 3)
+	while (space < size / 3)
 	{
-		for (i = gap; i < size; i++)
-		{
-			aux = array[i];
-			for (j = i; j >= gap && array[j - gap] > aux;
-					j -= gap)
-			{
-				if (array[j] != array[j - gap])
-					array[j] = array[j - gap];
-			}
-			if (array[j] != aux)
-				array[j] = aux;
+		space = space * 3 + 1;
+	}
 
+	while (space > 0)
+	{
+		for (i = space; i < size; i++)
+		{
+			tmp = array[i];
+			for (i = space; i < size; i += 1)
+			{
+				tmp = array[i];
+
+				j = i;
+				while (j >= space && array[j - space] > tmp)
+				{
+					array[j] = array[j - space], j -= space;
+				}
+				array[j] = tmp;
+			}
 		}
-		print_array(array, size);
+		print_array(array, size), space = (space - 1) / 3;
 	}
 }
