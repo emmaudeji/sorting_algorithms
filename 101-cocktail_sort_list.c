97f17e5a08f2a96c2f3a538c97c3fb6c7f46c947
emmaudeji <gogreneinfo@gmail.com>
2022-01-10 07:52:28 -0800

    cocktail sort

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -2,11 +2,9 @@
 
 /**
  * quick_sort - function that sorts an array of integers
- * in ascending order using the Quick sort algorithm
- * 
+ *              in ascending order using the Quick sort algorithm
  * @array: array
  * @size: array's size
- * 
  * Return: void
  */
 void quick_sort(int *array, size_t size)
